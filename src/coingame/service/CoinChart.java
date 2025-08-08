db6acb8fa0d0a8948229191e567d33e3aa305f8a
ruudska6 <feels19@naver.com>
2025-08-08 10:27:34 +0900

    feat: CoinChart 구현

--- src/coingame/service/CoinChart.java
+++ src/coingame/service/CoinChart.java
@@ -1,5 +1,16 @@
 package coingame.service;
 
-public class CoinChart {
+import java.util.ArrayList;
 
+public class CoinChart {
+	private ArrayList<Coin> coins = new ArrayList<>();
+	
+	public void showCoinList() {
+		"============구매할 수 있는 코인 목록입니다.========="
+		for (int i = 0; i < coins.length; i++) {
+			System.out.println("["+ i + "]" + coin);
+		}
+		
+		"============================================="
+	}
 }
