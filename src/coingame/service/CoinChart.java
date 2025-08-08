6245b5056c1fb4f2d69f11c99bd736e288aadbdf
ruudska6 <feels19@naver.com>
2025-08-08 12:42:56 +0900

    fix

--- src/coingame/service/CoinChart.java
+++ src/coingame/service/CoinChart.java
@@ -4,12 +4,25 @@ import java.util.ArrayList;
 
 import coingame.coin.Coin;
 
+// 1. 코인 차트에 있는 코인의 인덱스를 입력함
+// 2. buyCoin(int index, int amount)
+// 3. Wallet에 index에 있는 코인을 넣어줌. 
+
+
 public class CoinChart {	
 	private ArrayList<Coin> coins = new ArrayList<>();
     
 	// 코인 추가 메서드
     public void addCoin(Coin coin) {
         coins.add(coin);
+    }
+    
+    // 인덱스로 코인 가져오기 (Player가 구매할 때 사용)
+    public Coin getCoin(int index) {
+        if (index >= 0 && index < coins.size()) {
+            return coins.get(index);
+        }
+        return null;
     }
     
     
