2c63ce27bff0ed10aed0939c732ce5ba16d307a2
ruudska6 <feels19@naver.com>
2025-08-08 10:33:55 +0900

    Chore: CoinChart 오류수정정

--- src/coingame/service/CoinChart.java
+++ src/coingame/service/CoinChart.java
@@ -6,11 +6,12 @@ public class CoinChart {
 	private ArrayList<Coin> coins = new ArrayList<>();
 	
 	public void showCoinList() {
-		"============구매할 수 있는 코인 목록입니다.========="
+		System.out.println("============구매할 수 있는 코인 목록입니다.=========");
+	
 		for (int i = 0; i < coins.length; i++) {
 			System.out.println("["+ i + "]" + coin);
 		}
 		
-		"============================================="
+		System.out.println("=============================================");	
 	}
 }
