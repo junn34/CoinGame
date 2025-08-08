243cac663fabfb33728509d6f7fd1abfdb5e6341
ruudska6 <feels19@naver.com>
2025-08-08 11:15:12 +0900

    fix: CoinChart 버그 수정

--- src/coingame/service/CoinChart.java
+++ src/coingame/service/CoinChart.java
@@ -2,14 +2,16 @@ package coingame.service;
 
 import java.util.ArrayList;
 
+import coingame.coin.Coin;
+
 public class CoinChart {
 	private ArrayList<Coin> coins = new ArrayList<>();
 	
 	public void showCoinList() {
 		System.out.println("============구매할 수 있는 코인 목록입니다.=========");
 	
-		for (int i = 0; i < coins.length; i++) {
-			System.out.println("["+ i + "]" + coin);
+		for (int i = 0; i < coins.size(); i++) {
+			System.out.println("["+ i + "]" + coins.get(i));
 		}
 		
 		System.out.println("=============================================");	
